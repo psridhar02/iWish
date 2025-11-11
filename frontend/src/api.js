798ec2bc320fc39696573864c986eaa3960ed698
psridhar02 <preethisridhar02@gmail.com>
2025-11-11 15:31:51 -0500

    force fakestoreapi.com baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const hostname = window.location.hostname;
-const isLocal = hostname === "localhost" || hostname === "127.0.0.1";
-
 const API = axios.create({
-  baseURL: isLocal
-    ? "http://localhost:5000/api"
-    : "https://fakestoreapi.com", 
+  baseURL: "https://fakestoreapi.com",  
 });
 
 export default API;
